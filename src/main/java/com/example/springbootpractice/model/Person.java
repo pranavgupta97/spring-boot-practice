ca0db8f836b4620f056dcea21f8e856e60f4adc0
Pranav Gupta <pranavgupta9797@gmail.com>
2020-04-07 23:40:45 -0500

    Added data models

--- src/main/java/com/example/springbootpractice/model/Person.java
+++ src/main/java/com/example/springbootpractice/model/Person.java
@@ -1,4 +1,22 @@
 package com.example.springbootpractice.model;
 
+import java.util.UUID;
+
 public class Person {
+
+    private final UUID id;
+    private final String name;
+
+    public Person(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+}
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

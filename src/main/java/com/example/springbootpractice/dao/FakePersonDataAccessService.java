853b57829578458744d62b19a0d29b6997683a98
Pranav Gupta <pranavgupta9797@gmail.com>
2020-04-08 19:11:46 -0500

    Added Api Controller Methods

--- src/main/java/com/example/springbootpractice/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/springbootpractice/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -17,5 +18,54 @@ public class FakePersonDataAccessService implements PersonDao {
 
         dataBase.add(new Person(uuid, person.getName()));
         return 69;
+    }
+
+    @Override
+    public int updatePersonById(UUID uuid, Person person) {
+
+
+        return selectPersonById(uuid)
+                .map(currentPerson -> {
+                    int indexOfPersonToUpdate = dataBase.indexOf(currentPerson);
+
+                    if (indexOfPersonToUpdate >= 0) {
+                        dataBase.set(indexOfPersonToUpdate, currentPerson);
+                        return 1;
+                    }   else {
+                        return 0;
+                    }
+                })
+
+
+                .orElse(0);
+
+
+    }
+
+    @Override
+    public List<Person> selectAllPerson() {
+        return dataBase;
+    }
+
+    @Override
+    public int deletePersonById(UUID uuid) {
+        Optional<Person> optionalPerson = selectPersonById(uuid);
+
+        if (optionalPerson.isPresent()) {
+            dataBase.remove(optionalPerson.get());
+            return 1;
+        } else {
+            return 0;
+        }
+
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID uuid) {
+
+        return dataBase.stream()
+                .filter(person -> person.getId().equals(uuid))
+                .findFirst();
+
     }
 }
